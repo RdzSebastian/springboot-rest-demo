a3cc23d21c0584460fcaac951c20a5f0a97028be
RdzSebastian <rdzsebastian@gmail.com>
2022-08-11 16:31:57 -0300

    Start

---
+++ src/main/java/com/estonianport/springbootrestdemoangular/SpringbootRestDemoAngularApplication.java
@@ -0,0 +1,13 @@
+package com.estonianport.springbootrestdemoangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestDemoAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestDemoAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estonianport/springbootrestdemoangular/SpringbootRestDemoAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estonianport.springbootrestdemoangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestDemoAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
